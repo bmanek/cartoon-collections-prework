8f3afecc42d426a267ab67d885b1a2b85c225f53
Ben Manekin <ben.manekin@gmail.com>
2019-02-04 16:58:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,17 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each.with_index(1) do |dwarf, index| puts "#{index}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(elements)
+  elements.collect do |element| element.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(planeteers)
+  planeteers.any? do |planeteer|
+    planeteer.length > 4
+  end
 end
 
 def find_the_cheese# code an argument here
